2b1ae9232c144722e424056dc49c3998acb966a1
iamkorir3 <korir5956@gmail.com>
2025-08-26 23:01:37 +0300

    setting up env

--- my portfolio/src/components/Navigation.jsx
+++ my portfolio/src/components/Navigation.jsx
@@ -2,7 +2,26 @@ import { Navlink } from "react-router-dom";
 export default function Navigation() {
   return (
     <div>
-      <link></link>
+      <ul>
+        <li>
+          <Navlink to="">Home</Navlink>
+        </li>
+        <li>
+          <Navlink to="">About</Navlink>
+        </li>
+        <li>
+          <Navlink to="">Projects</Navlink>
+        </li>
+        <li>
+          <Navlink to="">Skills</Navlink>
+        </li>
+        <li>
+          <Navlink to="">Testimonials</Navlink>
+        </li>
+        <li>
+          <Navlink to="">Contact</Navlink>
+        </li>
+      </ul>
     </div>
   );
 }
