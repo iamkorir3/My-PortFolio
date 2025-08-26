053b25ea205dc77b6e8032dcf4e274114e9033b3
iamkorir3 <korir5956@gmail.com>
2025-08-27 00:42:54 +0300

    setting

--- my portfolio/src/App.jsx
+++ my portfolio/src/App.jsx
@@ -1,5 +1,9 @@
-function App() {
-  return <div></div>;
-}
+import Navigation from "./components/Navigation";
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
