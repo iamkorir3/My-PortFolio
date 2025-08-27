e4c151ee2df6d2839105d09a9df94b9cff19ab94
iamkorir3 <korir5956@gmail.com>
2025-08-27 23:00:02 +0300

    eslint

--- my portfolio/src/App.jsx
+++ my portfolio/src/App.jsx
@@ -3,7 +3,16 @@ import Navigation from "./components/Navigation";
 export default function App() {
   return (
     <div>
-      <Navigation />
+      <h1>
+        You travel the world.
+        <br />
+        WorldWise keeps track of your adventur.
+      </h1>
+      <h2>
+        A world map that tracks your footsteps into every city you can think of.
+        Never forget your wonderful experiences, and show your friends how you
+        have wandered the world.
+      </h2>
     </div>
   );
 }
